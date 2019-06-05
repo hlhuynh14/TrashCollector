dfee8d20337936798bd01cb9823e6c09786aa948
henry huynh <hlhuynh14@gmail.com>
2019-06-05 15:23:29 -0500

    add customers and employees tables

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        int id { get; set; }
+        string firstName { get; set; }
+        string lastName { get; set; }
+        string email { get; set; }
+        string address { get; set; }
+        int zipcode { get; set; }
+        int balance;
+        string pickUpDay;
+        DateTime oneTimePickUp;
+        DateTime suspendedStart;
+        DateTime supspendEnd;
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+
+
+    }
+}
---
+++ TrashCollector/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {
+        [Key]
+        int id { get; set; }
+        string firstName { get; set; }
+        string lastName { get; set; }
+        string email { get; set; }
+        string address { get; set; }
+        int zipcode { get; set; }
+        int balance;
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
